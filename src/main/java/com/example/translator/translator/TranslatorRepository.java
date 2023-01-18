1d01df5fca7472e0493381da8171e03f4408be42
teeto11 <elteeto11@gmail.com>
2023-01-18 07:17:08 +0000

    chore(TranslatorRepository): removing map initialization from constructor

--- src/main/java/com/example/translator/translator/TranslatorRepository.java
+++ src/main/java/com/example/translator/translator/TranslatorRepository.java
@@ -1,21 +1,25 @@
 package com.example.translator.translator;
 
-import org.apache.catalina.mapper.Mapper;
-
 import java.util.Map;
 
 public class TranslatorRepository {
     private Map<String, String> translations;
 
     public TranslatorRepository() {
-        this.translations = Map.of("hello", "wi", "bye", "wa");
+        //this.translations = Map.of("hello", "salut", "bye", "au revoir");
     }
 
-    public String getTranslation(String word) {
+    String Translations(String word) {
+
+        translations = Map.of("hello", "salut", "bye", "au revoir");
         if (translations.get(word) == null) {
             return "not found";
         } else {
             return translations.get(word);
         }
+    }
+
+    public String getTranslation(String word) {
+        return this.Translations(word);
     }
 }
